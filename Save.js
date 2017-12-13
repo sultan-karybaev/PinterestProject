978d318129171e5cc64245fa66e83e5be63b01b7
sultan-karybaev <sultan-karybaev@mail.ru>
2017-12-13 13:44:35 +0600

    Delete Save.js

--- Save.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var Save = mongoose.Schema({
-    coll: {type: mongoose.Schema.Types.ObjectId, ref: "Collection"},
-    image: {type: mongoose.Schema.Types.ObjectId, ref: "Image"},
-    imageWay: String
-}, { versionKey: false });
-
-module.exports = mongoose.model("Save", Save);
