39f9b1ee5d8946c18e86a8454d5c4bba2e961ab1
sultan-karybaev <sultan-karybaev@mail.ru>
2017-12-13 13:45:15 +0600

    Delete Collection.js

--- Collection.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var Collection = mongoose.Schema({
-    imagecount: Number,
-    title: String,
-    user: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
-}, { versionKey: false });
-
-module.exports = mongoose.model("Collection", Collection);
